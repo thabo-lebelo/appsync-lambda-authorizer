70e7732ba31c4824b5aa27448bfa2a52153d9c76
Thabo Lebelo <thabolebelo@icloud.com>
2022-03-27 20:19:31 +0200

    Lamda Stack

--- lib/appsync-lambda-authorizer-stack.ts
+++ lib/appsync-lambda-authorizer-stack.ts
@@ -1,16 +1,41 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
 
 export class AppsyncLambdaAuthorizerStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const vpc = new ec2.Vpc(this, "thabolebeloVPC", {
+            maxAzs: 2,
+            natGateways: 1,
+            subnetConfiguration: [
+                {
+                    name: 'private-subnet-1',
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+                    cidrMask: 24,
+                },
+                {
+                    name: 'public-subnet-1',
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    cidrMask: 24,
+                },
+            ],
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncLambdaAuthorizerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const lambdaFunction = new lambda.Function(this, 'lambda-function', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            vpc,
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+            },
+            memorySize: 1024,
+            timeout: Duration.seconds(300),
+            handler: 'index.handler',
+            functionName: 'AppSyncAuthorizer',
+            code: lambda.Code.fromAsset(path.join(__dirname, '/../src/')),
+        });
+    }
 }
